42f66017749df1cdac87b5ab1331d31c24c284b9
Matt Lehrer <lehrerm@gmail.com>
2021-11-18 00:10:37 +0100

    add build script

---
+++ scripts/build.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+
+spawn('npm', 'install', { cwd: './src/http/get-index' });
+spawn('npm', 'install', { cwd: './src/http/get-api-000seedandtype' });
