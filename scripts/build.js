c2c66cf0e790e0f025bcb755ca18274b49fcc30a
Matt Lehrer <lehrerm@gmail.com>
2021-11-18 00:12:28 +0100

    fix

--- scripts/build.js
+++ scripts/build.js
@@ -2,5 +2,5 @@
 
 const { spawn } = require('child_process');
 
-spawn('npm', 'install', { cwd: './src/http/get-index' });
-spawn('npm', 'install', { cwd: './src/http/get-api-000seedandtype' });
+spawn('npm', ['install'], { cwd: './src/http/get-index' });
+spawn('npm', ['install'], { cwd: './src/http/get-api-000seedandtype' });
